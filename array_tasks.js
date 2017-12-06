4b51f86cf054ddfaad1113a1cc494183e3aaf995
Huascar <huascarmc@hotmail.com>
2017-12-06 15:59:14 +0000

    concat function test passing

--- array_tasks.js
+++ array_tasks.js
@@ -1,35 +1,36 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
+	concat: function (arr1, arr2) {
+			array = arr1.concat(arr2);
+			return array;
+	},
 
-	// },
-
-	// insertAt: function (arr, itemToAdd, index) {
-		
-	// },
-
+	insertAt: function (arr, itemToAdd, index) {
+ 
+	},
+ //
 	// square: function (arr) {
-		
+ //
 	// },
-
+ //
 	// sum: function (arr) {
-		
+ //
 	// },
-
+ //
 	// findDuplicates: function (arr) {
-		
+ //
 	// },
-
+ //
 	// removeAndClone: function (arr, valueToRemove) {
-		
+ //
 	// },
-
+ //
 	// findIndexesOf: function (arr, itemToFind) {
-		
+ //
 	// },
-
+ //
 	// sumOfAllEvenNumbersSquared: function (arr) {
-		
+ //
 	// }
 
 }
